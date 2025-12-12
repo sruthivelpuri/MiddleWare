a2e7da3b92c12c03054b1c14e5b6caefe8548f21
sruthivelpuri <sruthivelpuri216@gmail.com>
2025-12-12 11:20:04 +0530

    12december

--- middleware.js
+++ middleware.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import axios from 'axios';
+import cors from 'cors';    
 const app = express();
+
+app.use(cors());
 
 //middleware function
 app.use(express.json());
@@ -19,3 +23,29 @@ app.post('/adduser',(req,res)=>{
 app.listen(7009,()=>{
     console.log("Middleware server started at port 7009");
 })
+
+
+
+
+//API calls
+
+const fetchdata = () =>{
+    axios.get("http://localhost:7009/").then((response)=>{
+        console.log(response.data);
+    }   ).catch((error)=>{
+        console.log(error);
+    }   );  
+}
+
+const postdata = () =>{
+    let user={
+        name:"John Doe",
+        age:30,
+        email:"XYZ@GMAIL.COM"
+    }
+    axios.post("http://localhost:7009/adduser",user).then((response)=>{
+        console.log(response.data);
+    }   ).catch((error)=>{
+        console.log(error);
+    }   );
+}
